042600212dde7a027e8bb62176008a894b71452a
Felipe Augusto Rosa <flp.far@gmail.com>
2021-08-27 06:14:14 -0300

    Refactor GenReport module

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -22,20 +22,6 @@ defmodule GenReport do
     |> Enum.reduce(report_acc(), &build_report/2)
   end
 
-  defp report_acc do
-    %{
-      all_hours: Enum.into(@names, %{}, &{&1, 0}),
-      hours_per_month:
-        Enum.into(@names, %{}, fn name ->
-          {name, Enum.into(DateHelper.months_list(), %{}, &{&1, 0})}
-        end),
-      hours_per_year:
-        Enum.into(@names, %{}, fn name ->
-          {name, Enum.into(DateHelper.years_list(), %{}, &{&1, 0})}
-        end)
-    }
-  end
-
   defp build_report([name, hours, _, month, year], %{
          all_hours: all_hours,
          hours_per_month: hours_per_month,
@@ -43,13 +29,34 @@ defmodule GenReport do
        }) do
     all_hours = Map.put(all_hours, name, all_hours[name] + hours)
 
-    hours_per_month_name =
-      Map.put(hours_per_month[name], month, hours_per_month[name][month] + hours)
-
-    hours_per_month = Map.put(hours_per_month, name, hours_per_month_name)
-    hours_per_year_name = Map.put(hours_per_year[name], year, hours_per_year[name][year] + hours)
-    hours_per_year = Map.put(hours_per_year, name, hours_per_year_name)
+    hours_per_month = update_submap(hours_per_month, name, month, hours)
+    hours_per_year = update_submap(hours_per_year, name, year, hours)
 
     %{all_hours: all_hours, hours_per_month: hours_per_month, hours_per_year: hours_per_year}
+  end
+
+  defp update_submap(map, key, subkey, value) do
+    submap = Map.put(map[key], subkey, map[key][subkey] + value)
+    Map.put(map, key, submap)
+  end
+
+  defp report_acc do
+    all_hours = build_map_from_list(@names, 0)
+
+    all_months = build_map_from_list(DateHelper.months_list(), 0)
+    hours_per_month = build_map_from_list(@names, all_months)
+
+    all_years = build_map_from_list(DateHelper.years_list(), 0)
+    hours_per_year = build_map_from_list(@names, all_years)
+
+    %{
+      all_hours: all_hours,
+      hours_per_month: hours_per_month,
+      hours_per_year: hours_per_year
+    }
+  end
+
+  defp build_map_from_list(list, value) do
+    Enum.into(list, %{}, &{&1, value})
   end
 end
