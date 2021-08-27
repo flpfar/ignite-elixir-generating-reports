019202eb97d8ac5ccef482eb6b05032932811fba
Felipe Augusto Rosa <flp.far@gmail.com>
2021-08-27 05:45:50 -0300

    Add hours_per_month report

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -17,18 +17,23 @@ defmodule GenReport do
   def build(filename) do
     filename
     |> GenReport.Parser.parse_file()
-    |> Enum.reduce(all_hours(), & calculate_hours/2)
+    |> Enum.reduce(report_acc(), & build_report/2)
   end
 
-  defp all_hours do
+  defp report_acc do
     %{
       all_hours: Enum.into(@names, %{}, & {&1, 0}),
+      hours_per_month: Enum.into(@names, %{}, fn name ->
+        {name, Enum.into(GenReport.Months.months_list, %{}, & {&1, 0})}
+      end)
     }
   end
 
-  defp calculate_hours([name, hours, _, _, _], %{all_hours: all_hours}) do
+  defp build_report([name, hours, _, month, _], %{all_hours: all_hours, hours_per_month: hours_per_month}) do
     all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+    hours_per_month_name = Map.put(hours_per_month[name], month, hours_per_month[name][month] + hours)
+    hours_per_month = Map.put(hours_per_month, name, hours_per_month_name)
 
-    %{all_hours: all_hours}
+    %{all_hours: all_hours, hours_per_month: hours_per_month}
   end
 end
