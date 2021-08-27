de242d3546ca6270951e907a1d453adea2256da1
Felipe Augusto Rosa <flp.far@gmail.com>
2021-08-27 05:30:47 -0300

    Add all_hours report

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,8 +1,34 @@
 defmodule GenReport do
+  @names [
+    "cleiton",
+    "daniele",
+    "danilo",
+    "diego",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "mayk",
+    "rafael",
+    "vinicius"
+  ]
+
   def build, do: {:error, "Insira o nome de um arquivo"}
 
   def build(filename) do
     filename
     |> GenReport.Parser.parse_file()
+    |> Enum.reduce(all_hours(), & calculate_hours/2)
+  end
+
+  defp all_hours do
+    %{
+      all_hours: Enum.into(@names, %{}, & {&1, 0}),
+    }
+  end
+
+  defp calculate_hours([name, hours, _, _, _], %{all_hours: all_hours}) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    %{all_hours: all_hours}
   end
 end
